f95f53eed6d5a08c6acb7d69b467983ef6422822
DiegoMaioneVallarino <diegopairtial@gmail.com>
2026-08-13 23:20:57 -0600

    Explore panel update

--- src/components/ExplorerPanel/ExplorerPanel.tsx
+++ src/components/ExplorerPanel/ExplorerPanel.tsx
@@ -10,7 +10,14 @@ function ExplorerPanel() {
           <div className="option-bar-bt"></div>
           <div className="option-bar-bt"></div>
           <div id="add-panel-alert" className="option-bar-bt">
-            <div className="option-bar-bt-ico">+</div></div></div>
+            <div className="option-bar-bt-ico">+</div>
+            <div className="option-inner-btadd"><div className="option-inner-btadd-tittle">Add new</div>
+            <div className="option-inner-btadd-addBtArea">
+              <div className="option-inner-btadd-addBtAreaBt"><div className="add-new-bloc-bt"></div></div>
+              <div className="option-inner-btadd-addBtAreaBt"><div className="add-new-bloc-bt"></div></div>
+              <div className="option-inner-btadd-addBtAreaBt"><div className="add-new-bloc-bt"></div></div><div className="option-inner-btadd-addBtAreaBt">
+                <div className="add-new-bloc-bt"></div></div></div></div>
+            </div></div>
       <div className="explorer-panel-alert color">
         <div className="type-of-explorer-panel-alert">
           <div className="text-explorer-panel-alert">
